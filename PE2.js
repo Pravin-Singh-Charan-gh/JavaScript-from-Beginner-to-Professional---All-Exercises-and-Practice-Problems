0ecb0588463a5e1e2282776b5293ebbb568947fd
Pravin Singh Charan <125742995+Pravin-Singh-Charan-gh@users.noreply.github.com>
2025-08-22 23:20:12 +0530

    Delete PE2.js

--- PE2.js
+++
@@ -1,5 +0,0 @@
-let name="Pravin Singh Charan";
-let age=21;
-let canCodeJS=true;
-let message=`Hello, my name is ${name}, I am ${age} years old and I can code JavaScript: ${canCodeJS}`;
-console.log(message);
